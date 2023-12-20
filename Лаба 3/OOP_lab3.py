ccd7a170b250ef743437fa59a1062608eedaa9e8
goroprav <56308902+goroprav@users.noreply.github.com>
2023-12-20 21:26:06 +0200

    Update OOP_lab3.py

--- Лаба 3/OOP_lab3.py
+++ Лаба 3/OOP_lab3.py
@@ -1,67 +1,70 @@
-import numpy as np
-
 class Array3d:
     def __init__(self, dim0, dim1, dim2):
         self.dim0 = dim0
         self.dim1 = dim1
         self.dim2 = dim2
-        self.array = np.zeros((dim0, dim1, dim2))
+        self.array = [[[0 for _ in range(dim2)] for _ in range(dim1)] for _ in range(dim0)]
 
     def __getitem__(self, idx):
         return self.array[idx]
 
     def GetValues0(self, i):
-        return self.array[i, :, :]
+        return self.array[i]
 
     def GetValues1(self, j):
-        return self.array[:, j, :]
+        return [[self.array[i][j] for i in range(self.dim0)] for _ in range(self.dim1)]
 
     def GetValues2(self, k):
-        return self.array[:, :, k]
+        return [[self.array[i][j][k] for j in range(self.dim1)] for i in range(self.dim0)]
 
     def GetValues01(self, i, j):
-        return self.array[i, j, :]
+        return self.array[i][j]
 
     def GetValues02(self, i, k):
-        return self.array[i, :, k]
+        return [self.array[i][j][k] for j in range(self.dim1)]
 
     def GetValues12(self, j, k):
-        return self.array[:, j, k]
+        return [self.array[i][j][k] for i in range(self.dim0)]
 
     def SetValues0(self, i, values):
-        self.array[i, :, :] = values
+        self.array[i] = values
 
     def SetValues1(self, j, values):
-        self.array[:, j, :] = values
+        for i in range(self.dim0):
+            self.array[i][j] = values[i]
 
     def SetValues2(self, k, values):
-        self.array[:, :, k] = values
+        for i in range(self.dim0):
+            for j in range(self.dim1):
+                self.array[i][j][k] = values[i][j]
 
     def SetValues01(self, i, j, values):
-        self.array[i, j, :] = values
+        self.array[i][j] = values
 
     def SetValues02(self, i, k, values):
-        self.array[i, :, k] = values
+        for j in range(self.dim1):
+            self.array[i][j][k] = values[j]
 
     def SetValues12(self, j, k, values):
-        self.array[:, j, k] = values
+        for i in range(self.dim0):
+            self.array[i][j][k] = values[i]
 
     @classmethod
     def ones(cls, dim0, dim1, dim2):
         instance = cls(dim0, dim1, dim2)
-        instance.array = np.ones((dim0, dim1, dim2))
+        instance.array = [[[1 for _ in range(dim2)] for _ in range(dim1)] for _ in range(dim0)]
         return instance
 
     @classmethod
     def zeros(cls, dim0, dim1, dim2):
         instance = cls(dim0, dim1, dim2)
-        instance.array = np.zeros((dim0, dim1, dim2))
+        instance.array = [[[0 for _ in range(dim2)] for _ in range(dim1)] for _ in range(dim0)]
         return instance
 
     @classmethod
     def fill(cls, dim0, dim1, dim2, value):
         instance = cls(dim0, dim1, dim2)
-        instance.array = np.full((dim0, dim1, dim2), value)
+        instance.array = [[[value for _ in range(dim2)] for _ in range(dim1)] for _ in range(dim0)]
         return instance
 
 
@@ -74,7 +77,12 @@ for i in range(arr.dim0):
             arr[i][j][k] = i * j * k
 
 # Выводим массив
-print(arr.array)
+for i in range(arr.dim0):
+    for j in range(arr.dim1):
+        for k in range(arr.dim2):
+            print(arr[i][j][k], end=" ")
+        print()
+    print()
 
 # Получаем и выводим срезы массива
 print(arr.GetValues0(1))
@@ -82,7 +90,7 @@ print(arr.GetValues1(1))
 print(arr.GetValues2(1))
 
 # Устанавливаем значения для срезов
-arr.SetValues0(1, np.ones((arr.dim1, arr.dim2)))
+arr.SetValues0(1, [[1 for _ in range(arr.dim2)] for _ in range(arr.dim1)])
 print(arr.GetValues0(1))
 
 # Создаем новые экземпляры класса с массивами, заполненными единицами, нулями и произвольным значением
@@ -91,6 +99,24 @@ arr_zeros = Array3d.zeros(3, 3, 3)
 arr_fill = Array3d.fill(3, 3, 3, 7)
 
 # Выводим эти массивы
-print(arr_ones.array)
-print(arr_zeros.array)
-print(arr_fill.array)
+for i in range(arr_ones.dim0):
+    for j in range(arr_ones.dim1):
+        for k in range(arr_ones.dim2):
+            print(arr_ones[i][j][k], end=" ")
+        print()
+    print()
+
+for i in range(arr_zeros.dim0):
+    for j in range(arr_zeros.dim1):
+        for k in range(arr_zeros.dim2):
+            print(arr_zeros[i][j][k], end=" ")
+        print()
+    print()
+
+for i in range(arr_fill.dim0):
+    for j in range(arr_fill.dim1):
+        for k in range(arr_fill.dim2):
+            print(arr_fill[i][j][k], end=" ")
+        print()
+    print()
+
